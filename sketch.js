60a36aacee1addb4673a9e0001a3f87ee9ebfb24
panchalneel2007 <77532273+panchalneel2007@users.noreply.github.com>
2021-02-05 18:42:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var fixedRect, movingRect;
+var car,wall;
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+  car = createSprite(200,200,40,40);
+  car.shapeColor = "blue";
+  car.velocityX = 4;
+  wall = createSprite(1000,200,20,100);
+  wall.shapeColor = "pink";
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if (isTouching(car,wall)){
+car.shapeColor = "pink";
+  }
+  drawSprites();
+}
+function isTouching(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object1.y < object2.height/2 + object1.height/2) {
+  return true;
+}
+else {
+  return false;
+}
+
+
+
+
+
+
+
+}
